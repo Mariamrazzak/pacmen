ba9705f79194054b13b050ef532e9ba624587b2e
mariam razzak <mariam.razzak@gmail.com>
2022-09-22 17:48:35 -0500

    Update pacmen.js

--- pacmen.js
+++ pacmen.js
@@ -1,6 +1,6 @@
 let pos = 0;
 const pacArray = [
-  ['images/PacMan1.png', ''images/PacMan2.png'],
+  ['images/PacMan1.png', 'images/PacMan2.png'],
   ['images/PacMan3.png', 'images/PacMan4.png'],
 ];
 let direction = 0;
